4ec2d29d0bbedd02df314e8d7b63237d33e11fb3
Marlone Akidiva <marlone911@gmail.com>
2024-09-04 21:09:51 +0300

    rebase changes to main

--- src/app/api/docs/route.ts
+++ src/app/api/docs/route.ts
@@ -1,51 +1,3 @@
-<<<<<<< Updated upstream
-import * as z from "zod"
-
-import { RequiresProPlanError } from "@/lib/exceptions"
-import { auth } from "@/auth"
-import * as service from "@/server/services/posts"
-
-export async function GET() {
-  try {
-    const session = await auth();
-
-    if (!session) {
-      return new Response("Unauthorized", { status: 403 })
-    }
-
-    const { user } = session
-    const posts = await service.getAllPosts(user)
-
-    return new Response(JSON.stringify(posts))
-  } catch (error) {
-    return new Response(null, { status: 500 })
-  }
-}
-
-export async function POST(req: Request) {
-  try {
-    const session = await auth();
-
-    if (!session) {
-      return new Response("Unauthorized", { status: 403 })
-    }
-
-    const { user } = session
-    const post = await service.createPost({ user, req })
-
-    return new Response(JSON.stringify(post))
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return new Response(JSON.stringify(error.issues), { status: 422 })
-    }
-
-    if (error instanceof RequiresProPlanError) {
-      return new Response("Requires Pro Plan", { status: 402 })
-    }
-
-    return new Response(null, { status: 500 })
-  }
-=======
 import { createDocHandler, getDocsHandler } from "@/server/controllers/docs";
 
 export async function GET() {
@@ -54,5 +6,4 @@ export async function GET() {
 
 export async function POST(req: Request) {
   return await createDocHandler(req);
->>>>>>> Stashed changes
 }
--- src/app/api/guides/[guideId]/route.ts
+++ src/app/api/guides/[guideId]/route.ts
@@ -1,80 +1,17 @@
 import * as z from "zod"
-<<<<<<< Updated upstream
-
-import { postPatchSchema } from "@/lib/validations/post"
-import * as service from "@/server/services/posts"
-
-const routeContextSchema = z.object({
-  params: z.object({
-    postId: z.string(),
-  }),
-})
-=======
 import { routeContextSchema } from "@/lib/schemas/post.schema";
 import { deleteGuideHandler, updateGuideHandler } from "@/server/controllers/guides";
->>>>>>> Stashed changes
 
 export async function DELETE(
   req: Request,
   context: z.infer<typeof routeContextSchema>
 ) {
-<<<<<<< Updated upstream
-  try {
-    // Validate the route params.
-    const { params } = routeContextSchema.parse(context)
-    const isAuthorized = await service.verifyCurrentUserHasAccessToPost(params.postId)
-
-    // Check if the user has access to this post.
-    if (!isAuthorized) {
-      return new Response(null, { status: 403 })
-    }
-
-    // Delete the post.
-    await service.deletePost({ params })
-
-    return new Response(null, { status: 204 })
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return new Response(JSON.stringify(error.issues), { status: 422 })
-    }
-
-    return new Response(null, { status: 500 })
-  }
-=======
   return await deleteGuideHandler(req, context);
->>>>>>> Stashed changes
 }
 
 export async function PATCH(
   req: Request,
   context: z.infer<typeof routeContextSchema>
 ) {
-<<<<<<< Updated upstream
-  try {
-    // Validate route params.
-    const { params } = routeContextSchema.parse(context)
-    const isAuthorized = await service.verifyCurrentUserHasAccessToPost(params.postId)
-
-    // Check if the user has access to this post.
-    if (!isAuthorized) {
-      return new Response(null, { status: 403 })
-    }
-
-    // Get the request body and validate it.
-    const json = await req.json()
-    const body = postPatchSchema.parse(json)
-
-    await service.update({ body, params })
-
-    return new Response(null, { status: 200 })
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return new Response(JSON.stringify(error.issues), { status: 422 })
-    }
-
-    return new Response(null, { status: 500 })
-  }
-=======
   return await updateGuideHandler(req, context);
->>>>>>> Stashed changes
 }
